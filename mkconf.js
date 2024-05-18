36c0d57cd587839cdcb7cf01625bd87434e0689c
Omi-chan <74772084+omi-coide@users.noreply.github.com>
2024-05-19 02:13:49 +0800

    add steps to reproduce config

---
+++ mkconf.js
@@ -0,0 +1,21 @@
+{
+	"data": "ENC[AES256_GCM,data:nl+AXLp0WHsbY4EqsrQpxSxfGYJ3uR/Tr2/kPa1wcSzbK1gP8jIMpMqY2HYqM1kBOr2V6WHyhofE9l8IRz8vnyQkPmHLaZBqsKHB2ohR6uvo2kAFODbwnx89n99JVDY3i9Uy5t0LM5mkrJyfyepeC7ZQM3dUNF8mkSRbY22vxGBJ2RVxjs6h5YYJ6sBEAqIBn888zQjgppjPjod2bc08shwLapver+MvEz9K1DQPchYAD6YAsVYijgffquPFFk6jqjmo7OOPuO8oYAJ8GefQin90sYQiZuLo9tFAc/Ryc0gfiaWmsQSM9f5v0Gkctn8j4BECk7IsDsc8LV0pZ538KexsAjw8iTaV9SCivYPocnkfvEHu9hwTAHyA+pbNxqBCGviwhdTixjeSXoCy8b0Oc95AuzqvvIOi77sojXvy2zzQEhAI6CqFGB+oG4s3WbXnipe4ChzSgR2/7ti25kK3B8mKy784NUpcBa0rMg66NzzgFcIhL0a3R/UpHmfaTH/H/ZB+SCh+pYjkMp7Yuv43kVmsjfnwv/81R0DkMluFw77IfvRQuspYBpxkdGvhoArdW5QczxMwdl8WCPUeQZu3NfWTr78Vxob7BZHtxe6uN1rpmZibzKmMdaZTXbgOrg00CfsLRC42QyTVtGzqUHt7EFT/g2nESJ1nsw/vYBwBzM6WF1xOOfh5d5cGIBr4OHmuDcDdoLCbCLrK0jTX4pvZG415lX4TFwvlIiJkuSU2rbEnjIQcA+TTaJHvRWUHWTZyGt2ghJgFNBUcWnSL4mL57nVMeF9DglTBkLRhPYjjeovM/N95xa+Mt9AoI9q/YV81nc0q77icq0NshjACWVfjKCQk1fXMDIWFNaiiMocNQhtBPE1/5wys9gXWRlDjqqdwxIt10h0QEAiZYwH40LgcsGpG1z7/QyU2ZAdXbHhORS4APsP/0t8CNGPEyp3BlBEeHfjHdYYrm1RilpRsQnNxjxUeipBlgiQP/vpSwozrHOy4EGOe0ktjRkIMDbCluzaIiMd0v+3bQ6BujeiWlXCz/EzgzmILoGu2JxbU1vtrr2+9FBuKM0ftYg+tNttjN1gFKg==,iv:mbmSa7ISfVn7rC7mwJGjX5yWscD/cqvVmrxQZd9eKaI=,tag:PkmWk7SsLseSzE7I7RZ1qg==,type:str]",
+	"sops": {
+		"kms": null,
+		"gcp_kms": null,
+		"azure_kv": null,
+		"hc_vault": null,
+		"age": null,
+		"lastmodified": "2024-05-18T18:12:44Z",
+		"mac": "ENC[AES256_GCM,data:1Qt97CAj7gAmVdnY5EYYU6NjASCXPrQwuT9Nq3JR0rME5F57oNK6ez+dRwVoiKIHqPUyarPLgBtzr1CMmGXQMSFZVVzMZeAlfE+LR7VjiY23SXmCaNYMXPLWz2czW4OW+UNQQBtOMl5PaAcwCMIddIMbFFmhgFM7ko3zX/qz3/8=,iv:NSO/vJuHN4mFbzpM3h90zGNQ/q9shZtWxpKQ1jmePks=,tag:RtoJgG4m6QQrlDw8QEbd2w==,type:str]",
+		"pgp": [
+			{
+				"created_at": "2024-05-18T18:12:44Z",
+				"enc": "-----BEGIN PGP MESSAGE-----\n\nhF4D5FJ9xkm/VEcSAQdApoQsT4j8sc6s3AcIB2OFAB7sMUOjtJhnDo/oBoEBT38w\nNbvP1uZgbe7bp4ApUMNG/glI6eYxbm5e6JU8aIAQm7o8UIAnAryuimDQ4GVmOB6k\n1GgBCQIQfZqvrF3JdNVc/Rt67bFp8abGqduNTEhs/YEtYZOa6KetHzRd3WZNS8f0\neds1J2THQBqThLu19CJ/ayZ6YVvIrD5COH7i/kHpo8JEHFoFfrnthl+ZvysdpMcX\nyV9UlJNK+sr5ug==\n=S1qy\n-----END PGP MESSAGE-----",
+				"fp": "36E5C1BF683129EF53BFFD4E8AB36B721C538E91"
+			}
+		],
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.8.1"
+	}
+}
